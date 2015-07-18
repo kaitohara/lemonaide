776fa1e22678f8f4355e491616ec77896b94b315
Kaito Hara <kaito.hara@me.com>
2015-07-18 15:29:17 -0400

    cleared Meaniscule

--- server/api/modules/index.js
+++
@@ -1,9 +0,0 @@
-var router = require('express').Router();
-
-var controller = require('./nodemodule.controller.js');
-
-module.exports = router;
-
-router.get('/', controller.index);
-
-router.post('/', controller.create);
