42347dd1853dea25454be5e9a6e5267a66fda423
Kaito Hara <kaito.hara@me.com>
2015-07-19 03:20:12 -0400

    youth route

--- public/javascripts/controllers/managerCtrl.js
+++ public/javascripts/controllers/managerCtrl.js
@@ -1,6 +1,13 @@
 app.controller('overviewCtrl', function($scope, overviewFactory) {
-	$scope.unitPrice = overviewFactory.getPrice();
-
+	
+	$scope.unitPrice;
+	$scope.numberSold;
+	overviewFactory.getPrice().then(function(data){
+		$scope.unitPrice = data;
+	});
+	overviewFactory.getNumberSold().then(function(data){
+		$scope.numberSold = data;
+	});
 	// $scope.unitPrice = 8;
 	$scope.dollarsEarned = function() {
 		return 5;
