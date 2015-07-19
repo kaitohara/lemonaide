d82db841ea3912f4c50e64fc0451c97fc2be4978
Kaito Hara <kaito.hara@me.com>
2015-07-19 05:05:56 -0400

    Made math on overview work

--- public/javascripts/controllers/managerCtrl.js
+++ public/javascripts/controllers/managerCtrl.js
@@ -1,12 +1,17 @@
 app.controller('overviewCtrl', function($scope, overviewFactory) {
-	
+	$scope.numberSoldToday = 0;
 	$scope.unitPrice;
-	$scope.numberSold;
+	$scope.prevNumberSold;
+	overviewFactory.getNumberSold().then(function(data){
+		$scope.prevNumberSold = data;
+	});
+
+	$scope.totalSold = function(){
+		console.log($scope.prevNumberSold, $scope.numberSoldToday)
+		return $scope.prevNumberSold + $scope.numberSoldToday;
+	}
 	overviewFactory.getPrice().then(function(data){
 		$scope.unitPrice = data;
-	});
-	overviewFactory.getNumberSold().then(function(data){
-		$scope.numberSold = data;
 	});
 	// $scope.unitPrice = 8;
 	$scope.dollarsEarned = function() {
