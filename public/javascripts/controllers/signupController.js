e9a37c9dec42aa6f23236803750ddb993e813437
Christian Evans <christian.evans214@gmail.com>
2015-07-19 12:13:32 -0400

    added charity signup screen

--- public/javascripts/controllers/signupController.js
+++ public/javascripts/controllers/signupController.js
@@ -1,11 +1,18 @@
 app.controller('signupCtrl', function($scope, $http) {
 	$scope.signupUser = {};
-	$scope.signUp = function(newKidData) {
-		console.log(newKidData);
+	$scope.signUp = function(charity, newKidData) {
+		// console.log(newKidData);
+		// console.log(charity);
+		newKidData.charityOfChoice = charity._id;
+		console.log("New Kid", newKidData)
 		$http.post("/youth", newKidData)
 			.then(function(data, status) {
 				console.log(data);
 			})
 	}
-
+	$http.get('/charities')
+		.then(function(data, status) {
+			$scope.charities = data;
+			console.log($scope.charities);
+		})
 })
