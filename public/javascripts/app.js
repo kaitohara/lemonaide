309b0f39b4e47ab704c5d03ab997a055156ac495
Christian Evans <christian.evans214@gmail.com>
2015-07-18 22:12:32 -0400

    signup states added

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -1,15 +1,34 @@
 var app = angular.module("lemonaide", ['ui.router']);
 
 app.config(function($stateProvider, $locationProvider) {
-	$locationProvider.html5Mode(true);
 	$stateProvider
 		.state("splashPage", {
-			url: "/",
+			url: "",
 			templateUrl: '/templates/splashPage.html'
 		})
-		// .state("login", {})
-		// .state("signup",{})
-		// .state("signup.section1", {})
-		// .state("signup.section2", {})
+		.state("login", {
+			url: "/login",
+			templateUrl: "/templates/login.html",
+		})
+		.state("signup", {
+			url: "/signup",
+			templateUrl: "/templates/signup.html"
+		})
+		.state("signup.name", {
+			url: "/name",
+			templateUrl: "/templates/signup-questions/name.html"
+		})
+		.state("signup.business-name", {
+			url: "/business-name",
+			templateUrl: "templates/signup-questions/business-name.html"
+		})
+		.state("signup.product", {
+			url: "/product",
+			templateUrl: "templates/signup-questions/product.html"
+		})
+		.state("signup.business-statement", {
+			url: "/business-statement",
+			templateUrl: "templates/signup-questions/business-statement.html"
+		})
 		// .state("dashboard", {})
 })
