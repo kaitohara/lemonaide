ce36e873d6825749197fba8af293ca7ab36c4dba
Robinnnnn <rokim8@gmail.com>
2015-07-19 05:32:22 -0400

    yoyoyo

--- public/javascripts/controllers/managerCtrl.js
+++ public/javascripts/controllers/managerCtrl.js
@@ -2,19 +2,27 @@ app.controller('overviewCtrl', function($scope, overviewFactory) {
 	$scope.numberSoldToday = 0;
 	$scope.unitPrice;
 	$scope.prevNumberSold;
-	overviewFactory.getNumberSold().then(function(data){
+	$scope.userName;
+	$scope.businessName;
+	overviewFactory.getNumberSold().then(function(data) {
 		$scope.prevNumberSold = data;
 	});
 
-	$scope.totalSold = function(){
+	$scope.totalSold = function() {
 		console.log($scope.prevNumberSold, $scope.numberSoldToday)
 		return $scope.prevNumberSold + $scope.numberSoldToday;
 	}
-	overviewFactory.getPrice().then(function(data){
+	overviewFactory.getPrice().then(function(data) {
 		$scope.unitPrice = data;
 	});
 	// $scope.unitPrice = 8;
 	$scope.dollarsEarned = function() {
 		return 5;
 	}
+	overviewFactory.getUsername().then(function(data) {
+		$scope.userName = data;
+	})
+	overviewFactory.getBusinessName().then(function(data) {
+		$scope.businessName = data;
+	})
 })
